b24ddfc1cb19611bf6fee3ca8bcd8ac2f2626d79
dreamofei <jiangyafeinet@126.com>
2015-10-08 17:40:38 +0800

    add

--- main/ElandBJ.ScheduledJob/Program.cs
+++ main/ElandBJ.ScheduledJob/Program.cs
@@ -12,14 +12,24 @@ namespace ElandBJ.ScheduledJob
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
-        static void Main()
+        //static void Main()
+        //{
+        //    ServiceBase[] ServicesToRun;
+        //    ServicesToRun = new ServiceBase[] 
+        //    { 
+        //        new EmailService() 
+        //    };
+        //    ServiceBase.Run(ServicesToRun);
+        //}
+
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-            { 
-                new EmailService() 
-            };
-            ServiceBase.Run(ServicesToRun);
+            Spring.Context.Support.ContextRegistry.GetContext();
+            Console.WriteLine("服务启动...");
+            //WriteTimer.Start();
+            (Spring.Context.Support.ContextRegistry.GetContext().GetObject("EmailManager") as ElandBJ.ScheduledJob.api.IEmailManager).InitEmailScheduler();
+            Console.WriteLine("启动成功");
+            Console.ReadKey();
         }
     }
 }
